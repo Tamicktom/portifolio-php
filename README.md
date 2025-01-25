09335da0305f5f56b1c1612c15816d6817969073
Tamicktom <rickpokemon38@gmail.com>
2025-01-25 14:35:03 -0300

    ok

---
+++ README.md
@@ -0,0 +1,10 @@
+# Portifolio - PHP
+
+Este é o projeto do meu portifolio em PHP.
+
+## Tecnologias
+
+- PHP
+- HTML
+- CSS
+- Tailwind CSS
--- api/index.php
+++ api/index.php
@@ -1,2 +1,16 @@
-<?php
-phpinfo();
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://unpkg.com/htmx.org@2.0.4"></script>
+  <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+  <title>Portifolio - PHP</title>
+</head>
+
+<body>
+  <h1>Portifolio - PHP</h1>
+</body>
+
+</html>
